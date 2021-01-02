f58a3e708c8ac49a103c7b94e056854c8b7ad634
tehnosia <70124459+tehnosia@users.noreply.github.com>
2021-01-03 01:12:30 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('portal-medis').then(function(cache) {
+     return cache.addAll([
+       '/pmpwa-instalable/',
+       '/pmpwa-instalable/index.html',
+       '/pmpwa-instalable/index.js',
+       '/pmpwa-instalable/style.css',
+       '/pmpwa-instalable/images/pmlogo.jpg',
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
