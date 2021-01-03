b6af744132135c095995cde09804ad47698c72c1
tehnosia <70124459+tehnosia@users.noreply.github.com>
2021-01-03 12:23:01 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,89 @@
-const CACHE_NAME=new Date().toISOString();const urlsToCache=["/css/main.min.css","/scripts/global.min.js","/scripts/page.min.js","/android-icon-192x192.png","/img/placeholder.svg","/img/placeholder-sqr.svg","/manifest.json"];self.addEventListener("install",function(event){event.waitUntil(caches.open(CACHE_NAME).then(function(cache){return cache.addAll(urlsToCache);}));});self.addEventListener("fetch",function(event){event.respondWith(caches.match(event.request,{cacheName:CACHE_NAME}).then(function(response){if(response){console.info("ServiceWorker: loading assets from cache ",response.url);return response;}
-console.info("ServiceWorker: loading assets from server ",event.request.url);return fetch(event.request);}));});self.addEventListener("activate",function(event){event.waitUntil(caches.keys().then(function(cacheNames){return Promise.all(cacheNames.map(function(cacheName){if(cacheName!=CACHE_NAME){console.info("ServiceWorker: cache "+cacheName+" deleted");return caches.delete(cacheName);}}));}));});
+self.addEventListener('push', function(event) {
+  var data = event.data.json(),
+      username = data.messageUserName || "",
+      message = data.messageText || "",
+      title = 'New message ' + (username?('from '+username):''),
+      location = data.location || '',
+      icon = data.iconPath || 'logo_freshchat.png',
+      body = message || 'You received a notification';
+
+  event.waitUntil(
+    self.registration.showNotification(title, {
+      body: body,
+      icon: icon,
+      data: location
+    })
+  );
+});
+
+function send_message_to_client(client, msg){
+  return new Promise(function(resolve, reject){
+    var msg_chan = new MessageChannel();
+
+    msg_chan.port1.onmessage = function(event){
+      if(event.data.error){
+        reject(event.data.error);
+      }else{
+        resolve({parentLocation: event.data, client: client});
+      }
+    };
+
+    client.postMessage(msg, [msg_chan.port2]);
+  });
+}
+
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+  var itemsProcessed = 0,
+      location = event.notification.data;
+
+  var promise = new Promise(function(resolve, reject) {
+        clients.matchAll({
+          type: "window"
+        })
+        .then(function(clientList) {
+          var promises = clientList.reduce(function(result, client) {
+            if(client.frameType === 'nested') {
+              var promise = new Promise(function(clientResolve, clientReject) {
+                send_message_to_client(client, "get_widget_location")
+                  .then(function(response) {
+                    itemsProcessed++;
+                    if(response.parentLocation === location) {
+                      return clientResolve(response.client);
+                    }
+                    if(itemsProcessed === clientList.length) {
+                      return clientReject("not_found");
+                    }
+                  }, function(reason) {
+                    itemsProcessed++;
+                  });
+              });
+              result.push(promise);
+            }
+            return result;
+          }, []);
+
+          if(promises && promises.length) {
+            Promise.race(promises).then(function(value){
+              resolve(value);
+            }, function(reason) {
+              reject(reason);
+            });
+          }
+          else {
+            reject("not_found");
+          }
+
+        })
+
+      }).then(function(client) {
+        client.focus();
+      }, function(reason) {
+        if (clients.openWindow) {
+          return clients.openWindow(location);
+        }
+      });
+
+  event.waitUntil(promise);
+
+});
